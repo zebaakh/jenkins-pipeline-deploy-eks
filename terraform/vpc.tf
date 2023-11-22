8ac7a2e916c823ea9168cce1720b57d6065a739b
zeba akh <131986592+zebaakh@users.noreply.github.com>
2023-11-23 01:19:30 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,7 +1,7 @@
 data "aws_availability_zones" "azs" {}
 module "myapp-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
-  version         = "3.19.0"
+  version         = "5.2.0"
   name            = "myapp-vpc"
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
